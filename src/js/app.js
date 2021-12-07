93133f0bddd3fb9d9058743b1da1406deebc1348
Kristiqn <kristiqn_estatiev@abv.bg>
2021-12-07 16:16:23 +0200

    listing the first 10 pokemon

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,13 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
+  .then(response => response.json())
+  .then(pokemonJSON => {
+    for(let pokemon of pokemonJSON.results) {
+      const li = document.createElement("li");
+      li.innerText = pokemon.name;
+      ul.appendChild(li);
+    }
+  });
 });
